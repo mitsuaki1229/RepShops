3f195e5c46be5ced1b39a79cf52f4ff1c87a737e
mitsuaki1229 <mitsuaki1229@gmail.com>
2022-03-04 21:57:01 +0900

    Add top view.

---
+++ RepShops/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RepShops
+//
+//  Created by Mitsuaki Ihara on 2022/03/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RepShops/RepShopsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepShopsApp.swift
+//  RepShops
+//
+//  Created by Mitsuaki Ihara on 2022/03/04.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
